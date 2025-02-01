e73a8d0fc594429fec1de9ace1a6cf4da263723f
suleymannmeral <mrlslymn02@gmail.com>
2025-02-01 22:10:27 +0300

    CalculateTheFactorial

---
+++ calculateFactorial(2)/Program.cs
@@ -0,0 +1,27 @@
+﻿// A Program that calcualtes the factorial value of the entered number
+
+Console.Write("Please enter a number : ");
+int number= Convert.ToInt32(Console.ReadLine());
+
+int factorial = 1;
+
+for(int i=1;i<=number;i++)
+{
+    factorial= factorial * i;
+}
+
+if(factorial==0)
+{
+    factorial = 1;
+}
+
+Console.Write("Factorial value is :"+factorial);
+
+
+
+
+
+
+
+
+
