d0bb29c62196f6959ecd312d91b41b1b307a7353
suleymannmeral <mrlslymn02@gmail.com>
2025-02-03 11:51:57 +0300

    factorial-2

---
+++ calculateFactorial_2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+while (true)
+{
+    Console.Write("Please Enter a number");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int factorial = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        factorial = factorial * i;
+
+    }
+
+
+    Console.WriteLine("Factorial value is : " + factorial);
+
+    Console.Write("Do u want to try again : (1/0)");
+    int reply = Convert.ToInt32(Console.ReadLine());
+
+    if(reply == 0)
+    {
+        Console.Write("Exit..");
+        break;
+    }
+
+
+}
+
