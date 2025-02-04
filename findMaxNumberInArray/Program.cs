f6d42a9d14c6562beae74394c6f1e24acf02c4c4
suleymannmeral <mrlslymn02@gmail.com>
2025-02-04 17:46:51 +0300

    find the maxNumberInArray (3)

---
+++ findMaxNumberInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.Write("Please enter a value of array size : ");
+
+int size = int.Parse(Console.ReadLine());
+
+int[] array = new int[size];
+
+for (int i=0; i<size;i++)                           
+{
+    Console.Write($"{i+1}. Number is  : ");
+    array[i] = int.Parse(Console.ReadLine());        
+}
+
+
+int maxNumber= array[0];
+
+foreach(int num in array)
+{
+    if(num>maxNumber)
+    {
+        maxNumber = num; 
+    }
+}
+Console.Write("Max Number Is : " + maxNumber);
