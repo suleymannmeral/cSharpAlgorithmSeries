bb0a7239ebc9cffc77872f50bb2e1efa7a51dd36
suleymannmeral <mrlslymn02@gmail.com>
2025-02-05 12:59:50 +0300

    Algorithm_5

---
+++ algorithm_5/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithmCSharp_5_
+{
+    public class Class1
+    {
+        public int Algorithm(int x , int y)
+        {
+            return x == y ? (x + y) * 3 : x + y;
+        }
+       
+    }
+}
---
+++ algorithm_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using algorithmCSharp_5_;
+
+
+
+
+Class1 obj = new Class1();
+
+
+
+Console.Write("Enter two numbers separated by space: ");          
+
+string input=Console.ReadLine();
+
+string[] array = input.Split(' ');
+
+int num1 = int.Parse(array[0]); 
+int num2 = int.Parse(array[1]);
+
+Console.Write(obj.Algorithm(num1, num2));
+
+
