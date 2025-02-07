9c9798cf7b4286c2d674482e8216fa6214adcbfa
suleymannmeral <mrlslymn02@gmail.com>
2025-02-07 14:14:26 +0300

    Algorithm6 added

---
+++ algorithm6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Write a C# Sharp program to create a string with the last char added at the front and back of a given string of length 1 or more.
+
+
+using csAlgorithm6;
+
+Console.Write("Please enter a word: ");
+string word = Console.ReadLine();
+Alogrithm6 alogrithm6 = new Alogrithm6();
+Console.Write(alogrithm6.algoritmhm(word));
+
