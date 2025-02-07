c83c2092731d5f2fd1414e4a79029c5d2e1bd91c
suleymannmeral <mrlslymn02@gmail.com>
2025-02-07 15:48:51 +0300

    Algorithm7 added

---
+++ algorithm_7/Algorithm7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestIntegersAmongThree
+{
+    public  class Algorithm7
+    {
+        public  int FindTheLargestNumber(int x, int y, int z)
+        {
+            int maxNumber = Math.Max(x,Math.Max(y,z));
+
+
+            return maxNumber;
+        }
+
+    }
+}
---
+++ algorithm_7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Write a C# program to check the largest number among three given numbers   u can use math func
+using LargestIntegersAmongThree;
+
+Console.Write("Please enter 3 number seperated by , : ");
+string input = Console.ReadLine();
+
+string[] array = input.Split(',');
+
+int num1 = int.Parse(array[0]);
+int num2 = int.Parse(array[1]);
+int num3 = int.Parse(array[2]);
+
+Algorithm7 algorithm7 = new Algorithm7();
+Console.Write(algorithm7.FindTheLargestNumber(num1, num2, num3));
+
+
