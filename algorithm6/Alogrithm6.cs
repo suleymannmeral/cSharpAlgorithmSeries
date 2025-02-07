ddb9403224926b4af32a10cce534f4cd6c9860cb
suleymannmeral <mrlslymn02@gmail.com>
2025-02-07 14:16:30 +0300

    Algorithm6 added-1

---
+++ algorithm6/Alogrithm6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csAlgorithm6
+{
+      public class Alogrithm6
+    {
+        public string algoritmhm(string text)     
+        {
+          
+           var s = text.Substring(text.Length-1);
+
+
+            return s+text+s;
+        }
+    }
+}
