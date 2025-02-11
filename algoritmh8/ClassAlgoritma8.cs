5f06b64e6b720b771812922ad278cbbe554fcf5f
suleymannmeral <mrlslymn02@gmail.com>
2025-02-11 20:02:23 +0300

    algorithm8

---
+++ algoritmh8/ClassAlgoritma8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm8
+{
+    public  class ClassAlgoritma8
+    {
+        public static int[] Rotate(int[] x)
+        {
+            int left=0, right=x.Length-1;
+
+            while (left < right)
+            {
+                int temp = x[left];
+                x[left] = x[right];
+                x[right] = temp;
+
+                left++;
+                right--;
+            }
+            return x;
+
+        }
+    }
+}
---
+++ algoritmh8/Program.cs
@@ -0,0 +1,16 @@
+﻿//Write a C# program that returns the elements of a given integer array in the opposite direction
+
+using Algorithm8;
+
+Console.Write("Please enter a array seperated by comma , :  ");
+string input  = Console.ReadLine();
+
+int[] array = input.Split(',').Select(int.Parse).ToArray();
+
+int[] result = ClassAlgoritma8.Rotate(array);
+
+foreach (int i in result)
+{
+    Console.Write(i);
+    Console.Write(",");
+}
