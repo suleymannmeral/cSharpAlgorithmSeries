9f39ab95aae08836e942d99be27f65deb9e9d55b
suleymannmeral <mrlslymn02@gmail.com>
2025-02-17 18:55:30 +0300

    Algorithm_9

---
+++ algorithm_9/Algoritma9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_9
+{
+    public class Algoritma9
+    {
+        public string Algorithm9(string text)
+        {
+            string threeCopy = $"{text[0]}{text[1]}";
+            return String.Concat(Enumerable.Repeat(threeCopy, 3));
+           
+
+        }
+
+    }
+}
---
+++ algorithm_9/Program.cs
@@ -0,0 +1,9 @@
+﻿//Write a C# Sharp program to create a new string using 3 copies of the first 2
+//characters of a given string. If the string length is less than 2 use the whole string.
+
+using Algorithm_9;
+
+Algoritma9 algoritma9 = new Algoritma9();
+Console.WriteLine("Please enter a string");
+string input = Console.ReadLine();
+ Console.Write(algoritma9.Algorithm9(input));
